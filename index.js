48ee1e3cfbefdb3f58c9ace10047f2969737d2a8
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-07 13:24:55 +0530

    add function to generate random Hex color values in index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// generating random Hex Values
+
+const randomColor = () => {
+    const hex = '0123456789ABCDEF'
+    let color = '#'
+
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+
+    return color
+}
+
